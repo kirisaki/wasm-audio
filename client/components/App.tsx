0257e33b292d002032cc418d557b83922f1ec973
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-02-22 20:21:45 +0900

    It works!!!

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react'
+import { useEffect } from 'react'
 import { Slidebar } from './Slidebar'
 import { Switch } from './Switch'
+import {Oscillator} from '../../pkg'
 
-export class App extends React.Component<{}, {}> {
-  render() {
-    return (
-      <main>
-        <Slidebar />
-        <Switch />
-      </main>
-    )
-  }
+export const App = ({ wasm }) => {
+  useEffect (() => {
+    const osc = new wasm.Oscillator()
+    osc.set_gain(0.02)
+  })
+  return (
+    <main>
+      <Slidebar />
+      <Switch />
+    </main>
+  )
 }
